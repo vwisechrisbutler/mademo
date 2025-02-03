b33f1468084cdffeb5430ce4bf1358a562106da2
vwisechrisbutler <121135840+vwisechrisbutler@users.noreply.github.com>
2025-02-03 12:16:34 -0800

    partials

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <?php include('components/languageSelector.php'); ?>
+    <?php include('components/fine-print.php'); ?>
+</footer>
---
+++ head.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>vWise Managed Accounts</title>
+<link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,700,900|Open+Sans:300,400' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="../assets/css/powerview.css?ver=<?php echo microtime(); ?>" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+</head>
