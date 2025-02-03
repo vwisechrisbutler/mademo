23c56768277815214acd4e488c63d835d14fe0fb
vwisechrisbutler <121135840+vwisechrisbutler@users.noreply.github.com>
2025-02-03 12:15:49 -0800

    rtd

---
+++ data.php
@@ -0,0 +1,23 @@
+<?php
+
+$data = [
+    'firstName' => 'Mark',
+    'lastName' => 'Doe',
+    'planName' => 'Sample Company Retirement Plan',
+    'balance' => '90,386',
+    'currentYTDTotal' => '6,000',
+    'currentYTDMatch' => '3,000',
+    'maxMatchParticipant' => '9,000',
+    'maxMatchEmployer' => '6,000',
+    'currentDeferral' => '3',
+    'projectedIncome' => 730528,
+    'retireAge' => '65',
+    'projectedMonthlyIncome' => 2538,
+    'maxMatch' => '5',
+    'projectedIncomeMax' => '1,248,000',
+    'projectedMonthlyIncomeMax' => '4,620',
+    'diffDeferral' => '5',
+    'confirmationCode' => '1340984-34234',
+];
+
+?>
